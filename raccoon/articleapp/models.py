e25168bde68d411582a20ae9e8aca815f355f49b
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-16 14:28:52 +0900

    User, Post, Tagモデル追加

--- raccoon/articleapp/models.py
+++ raccoon/articleapp/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class User(AbstractUser):
+    display_name = models.CharField(max_length=100, null=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(to="Tag", blank=True)
+    is_published = models.BooleanField(default=False)
+    date_publish = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def publish(self):
+        """
+        投稿のステータスを公開中に切り替えて投稿日を記録する
+        """
+        if self.is_published:
+            return
+        self.is_published = True
+        self.date_publish = timezone.now().date()
+        self.save()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
