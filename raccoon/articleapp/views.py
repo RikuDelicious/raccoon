b93802ece731c2c12989916ad515d211a5c0529b
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-20 16:56:19 +0900

    リクエストによってタイトルを変更

--- raccoon/articleapp/views.py
+++ raccoon/articleapp/views.py
@@ -7,9 +7,8 @@ def index(request):
     # tagをランダムに10個取得
     # order_by("?")でランダムに並べ替え
     tags = (
-        Tag.objects
-        .filter(post__isnull=False)  # 少なくとも1つ以上の投稿があるタグのみにする
-        .filter(post__is_published=True) # 少なくとも1つ以上の投稿が公開されているタグのみにする
+        Tag.objects.filter(post__isnull=False)  # 少なくとも1つ以上の投稿があるタグのみにする
+        .filter(post__is_published=True)  # 少なくとも1つ以上の投稿が公開されているタグのみにする
         .order_by("?")[0:10]
     )
 
@@ -18,5 +17,14 @@ def index(request):
 
     return render(request, "articleapp/index.html", {"posts": posts, "tags": tags})
 
+
 def search(request):
-    return render(request, "articleapp/search.html")
+    querydict = request.GET
+    context = {}
+
+    if "title" in querydict:
+        context["title"] = querydict["title"]
+    else:
+        context["title"] = "記事を検索"
+
+    return render(request, "articleapp/search.html", context)
