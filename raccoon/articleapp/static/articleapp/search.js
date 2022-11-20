ffcb3e5c1f2057754137dcf8e513120a82cb4478
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-20 18:21:58 +0900

    「期間」選択時の表示処理追加

--- raccoon/articleapp/static/articleapp/search.js
+++ raccoon/articleapp/static/articleapp/search.js
@@ -1,7 +1,7 @@
 'use strict';
 {
+    // 「フィルタ」ボタン押下時のアイコンアニメーションとアコーディオン開閉処理
     $(() => {
-        // 「フィルタ」ボタン押下時のアイコンアニメーションとアコーディオン開閉処理
         let inputs = $('#filter_period_input, #filter_tag_input, #filter_sort_input');
         let arrow_svg_anim = document.querySelector('#button_filter_accordion svg').animate(
             [
@@ -22,6 +22,28 @@
         $('#button_filter_accordion').click((e) => {
             arrow_svg_anim.play()
             inputs.slideToggle();
+        });
+    });
+
+    // 「期間」選択時の入力欄のアクティブ・非アクティブ切り替え
+    $(() => {
+        let period_start_date = $('#period_start_date')[0];
+        let period_start_date_label = $('label[for=period_start_date]');
+        let period_end_date = $('#period_end_date')[0];
+        let period_end_date_label = $('label[for=period_end_date]');
+        $('input[name=period]').change((e) => {
+            let checked = $('input[name=period]:checked');
+            if (checked.attr('id') === 'specify_period') {
+                period_start_date.toggleAttribute("disabled", false);
+                period_start_date_label.toggleClass('text-[#E6E6E6]', true);
+                period_end_date.toggleAttribute("disabled", false);
+                period_end_date_label.toggleClass('text-[#E6E6E6]', true);
+            } else {
+                period_start_date.toggleAttribute("disabled", true);
+                period_start_date_label.toggleClass('text-[#E6E6E6]', false);
+                period_end_date.toggleAttribute("disabled", true);
+                period_end_date_label.toggleClass('text-[#E6E6E6]', false);
+            }
         });
     });
 }
