fc32640609082556a19116c934af10e720527ef9
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-21 17:44:06 +0900

    タグのスタイル調整

--- raccoon/articleapp/static/articleapp/search.js
+++ raccoon/articleapp/static/articleapp/search.js
@@ -78,7 +78,7 @@
                 let li = $('<li>');
                 let button = $('<button>', {
                     type: "button",
-                    text: tag.name,
+                    text: '+ ' + tag.name,
                 });
                 button.click((e) => {
                     // フィルタ追加
@@ -104,17 +104,19 @@
             const tag_filter_list = $('#tag_filter_list');
             tag_filter_list.empty();
             CONTEXT["tags_filter"].forEach((tag) => {
-                let li = $('<li>');
+                let li = $('<li>', { 'class': 'tag_filter_item' });
+                let span = $('<span>', { text: tag.name });
                 let button = $('<button>', {
                     type: "button",
-                    text: tag.name,
                 });
+                let x_mark = $('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>');
                 button.click((e) => {
                     CONTEXT.tags_filter_remove(tag);
                     updateTagsFilterView();
                     updateTagsSourceView();
                 });
-                li.append(button);
+                button.append(x_mark);
+                li.append(span, button);
                 tag_filter_list.append(li);
             });
         }
