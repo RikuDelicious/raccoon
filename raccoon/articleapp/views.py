69485c2b203ec20e0e0b58551d2135560f4acd34
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-22 15:58:08 +0900

    search viewのフィルタ追加

--- raccoon/articleapp/views.py
+++ raccoon/articleapp/views.py
@@ -1,5 +1,10 @@
+import datetime
+
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils import timezone
 
 from .models import Post, Tag
 
@@ -21,8 +26,65 @@ def index(request):
 
 
 def search(request):
+    queryset = Post.objects.all()
     querydict = request.GET
     context = {}
+
+    # フィルタ: タグ
+    if "tags" in querydict:
+        tags = querydict.getlist("tags")
+        for tag in tags:
+            queryset = queryset.filter(tags__name=tag)
+
+    # フィルタ: 期間_開始
+    if "period_start_date" in querydict:
+        try:
+            start_date = timezone.make_aware(
+                datetime.datetime.strptime(querydict["period_start_date"], "%Y/%m/%d")
+            )
+            queryset = queryset.filter(date_publish__gte=start_date)
+        except ValueError:
+            # datetimeに変換できない値はスルー
+            pass
+
+    # フィルタ:期間_終了
+    if "period_end_date" in querydict:
+        try:
+            end_date = timezone.make_aware(
+                datetime.datetime.strptime(querydict["period_end_date"], "%Y/%m/%d")
+            )
+            end_date = end_date + datetime.timedelta(days=1)
+            queryset = queryset.filter(date_publish__lt=end_date)
+        except ValueError:
+            # datetimeに変換できない値はスルー
+            pass
+
+    # フィルタ:検索キーワード
+    if "keyword" in querydict:
+        keywords = querydict["keyword"].split()  # 全角/半角スペースで区切る
+        for keyword in keywords:
+            queryset = queryset.filter(
+                Q(title__icontains=keyword) | Q(body__icontains=keyword)
+            )
+
+    # フィルタ: 並び順
+    # デフォルトは投稿日の降順
+    queryset = queryset.order_by("-date_publish")
+    if "sort" in querydict:
+        sort_value = querydict["sort"]
+        if sort_value == "date_publish_desc":
+            pass  # 何もしない
+        if sort_value == "date_publish_asc":
+            queryset = queryset.order_by("date_publish")
+
+    # ページネーション
+    paginator = Paginator(queryset, 10)
+
+    page_number = 1
+    if "page" in querydict:
+        page_number = int(querydict["page"])
+    page = paginator.get_page(page_number)
+    context["post_list_page"] = page
 
     return render(request, "articleapp/search.html", context)
 
