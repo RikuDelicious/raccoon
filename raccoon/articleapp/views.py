cdd2f852c18d424f40d6d70ebca76ea9157b259e
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-22 23:33:03 +0900

    ページネーション機能実装

--- raccoon/articleapp/views.py
+++ raccoon/articleapp/views.py
@@ -79,12 +79,51 @@ def search(request):
 
     # ページネーション
     paginator = Paginator(queryset, 10)
-
     page_number = 1
     if "page" in querydict:
-        page_number = int(querydict["page"])
+        try:
+            page_number = int(querydict["page"])
+        except ValueError:
+            # intに変換できない値はスルー
+            pass
     page = paginator.get_page(page_number)
     context["post_list_page"] = page
+
+    # ページネーションのナビゲーションに表示する番号を予め決めておく
+    pagination_nav_numbers = []
+    pagination_nav_display_first = False
+    pagination_nav_display_last = False
+
+    # 現在のページを中心として前後最大5ページ分を表示する
+    # 上記の内、最初・最後のページを優先して表示する
+    if paginator.num_pages >= 5:
+        # 最初に、現在のページを真ん中にする
+        left = page.number - 2
+        right = page.number + 2
+        # 左側か右側がページ範囲を逸脱する場合、その分を他方の側に加える
+        if left < 1:
+            right = right + (1 - left)
+            left = 1
+        elif right > paginator.num_pages:
+            left = left - (right - paginator.num_pages)
+            right = paginator.num_pages
+        pagination_nav_numbers = list(range(left, right + 1))
+        # 左端が2以上の場合、左端の代わりに最初のページを表示する
+        if left > 1:
+            pagination_nav_numbers.remove(left)
+            pagination_nav_display_first = True
+        # 右端が最後のページの1個前以下の場合、代わりに最後のページを表示する
+        if right < paginator.num_pages:
+            pagination_nav_numbers.remove(right)
+            pagination_nav_display_last = True
+    else:
+        pagination_nav_numbers = list(range(1, paginator.num_pages + 1))
+
+    context["post_list_pagination_nav"] = {
+        "numbers": pagination_nav_numbers,
+        "display_first": pagination_nav_display_first,
+        "display_last": pagination_nav_display_last,
+    }
 
     return render(request, "articleapp/search.html", context)
 
