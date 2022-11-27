10ffc96d322f7406dd25de6dd1c69a45b70ec2aa
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-28 00:20:59 +0900

    フィルターフォーム要素から要素取得_アコーディオン

--- raccoon/articleapp/static/articleapp/search.js
+++ raccoon/articleapp/static/articleapp/search.js
@@ -3,31 +3,30 @@
     $(() => {
         const FILTER_FORM = $('#filter_form');
 
-    });
-
-    // 「フィルタ」ボタン押下時のアイコンアニメーションとアコーディオン開閉処理
-    $(() => {
-        let inputs = $('#filter_period_input, #filter_tag_input, #filter_sort_input');
-        let arrow_svg_anim = document.querySelector('#button_filter_accordion svg').animate(
-            [
-                { transform: 'rotate(0deg)' },
-                { transform: 'rotate(180deg)' }
-            ],
-            {
-                duration: 200,
-                fill: 'forwards'
-            }
-        );
-        arrow_svg_anim.addEventListener('finish', (e) => {
-            arrow_svg_anim.reverse();
+        // 「フィルタ」ボタン押下時のアイコンアニメーションとアコーディオン開閉処理
+        {
+            let inputs = FILTER_FORM.find('#filter_period_input, #filter_tag_input, #filter_sort_input');
+            let arrow_svg_anim = FILTER_FORM.find('#button_filter_accordion svg')[0].animate(
+                [
+                    { transform: 'rotate(0deg)' },
+                    { transform: 'rotate(180deg)' }
+                ],
+                {
+                    duration: 200,
+                    fill: 'forwards'
+                }
+            );
+            arrow_svg_anim.addEventListener('finish', (e) => {
+                arrow_svg_anim.reverse();
+                arrow_svg_anim.pause();
+            });
             arrow_svg_anim.pause();
-        });
-        arrow_svg_anim.pause();
-
-        $('#button_filter_accordion').click((e) => {
-            arrow_svg_anim.play()
-            inputs.slideToggle();
-        });
+    
+            FILTER_FORM.find('#button_filter_accordion').click((e) => {
+                arrow_svg_anim.play()
+                inputs.slideToggle();
+            });
+        }
     });
 
 
