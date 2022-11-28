1290ad30799e9d46534419e4563559b47cfe847b
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-28 15:18:25 +0900

    import分整形

--- raccoon/articleapp/models.py
+++ raccoon/articleapp/models.py
@@ -1,7 +1,9 @@
+import random
+import string
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
-import random, string
 
 
 def generate_random_slug():
