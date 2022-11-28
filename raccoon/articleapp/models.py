4ca10ff7752d0b6591dd271f4f0068af5617ff49
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-28 17:33:27 +0900

    ユーザー名をASCII文字だけにする

--- raccoon/articleapp/models.py
+++ raccoon/articleapp/models.py
@@ -2,9 +2,11 @@ import random
 import string
 
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 def generate_random_slug():
@@ -16,6 +18,20 @@ def generate_random_slug():
 # Create your models here.
 class User(AbstractUser):
     display_name = models.CharField(max_length=100, null=True)
+
+    username_validator = ASCIIUsernameValidator
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
 
 
 class Post(models.Model):
