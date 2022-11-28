03d616d08517e792923d816f875f99dd96d3d93e
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-28 23:01:58 +0900

    LoginViewTests 追加

--- raccoon/articleapp/tests.py
+++ raccoon/articleapp/tests.py
@@ -888,3 +888,21 @@ class SignUpViewTests(TestCase):
         self.assertEqual(response.resolver_match.func, views.signup)
         self.assertNotEqual(response.context["form"].errors.as_data(), {})
         self.assertFalse(User.objects.filter(username="あかさたな").exists())
+
+
+class LoginViewTests(TestCase):
+    def setUp(self):
+        self.url = reverse_lazy("login")
+
+    def test_未ログインでページアクセス(self):
+        c = Client()
+        response = c.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_ログイン済みでアクセスするとリダイレクト(self):
+        User.objects.create_user(username="testuser_1", password="testuser_1")
+        c = Client()
+        c.login(username="testuser_1", password="testuser_1")
+        response = c.get(self.url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.resolver_match.func, views.index)
