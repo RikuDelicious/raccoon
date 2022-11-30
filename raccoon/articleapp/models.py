d69f81e0a813231be9c711b520c5a3052724c799
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-12-01 00:19:14 +0900

    Userモデルにverbose_name追加

--- raccoon/articleapp/models.py
+++ raccoon/articleapp/models.py
@@ -17,8 +17,10 @@ def generate_random_slug():
 
 # Create your models here.
 class User(AbstractUser):
-    display_name = models.CharField(max_length=100, null=True)
-    profile_image = models.ImageField(upload_to="uploads/", null=True)
+    display_name = models.CharField(max_length=100, null=True, verbose_name="ニックネーム")
+    profile_image = models.ImageField(
+        upload_to="uploads/", null=True, verbose_name="プロフィール画像"
+    )
 
     username_validator = ASCIIUsernameValidator()
     username = models.CharField(
