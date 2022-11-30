d44b305ae2be13e590f3233f64d8db75515ece94
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-11-30 23:00:52 +0900

    ユーザー情報変更用のフォーム作成

--- raccoon/articleapp/forms.py
+++ raccoon/articleapp/forms.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
+from django.forms import ModelForm
+
 from .models import User
 
 
 class UserCreationForm(BaseUserCreationForm):
     class Meta(BaseUserCreationForm.Meta):
         model = User
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "display_name", "profile_image"]
