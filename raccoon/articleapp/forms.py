117c0a1b5e8c8316490639a1008e32d7f2017155
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-12-01 01:54:20 +0900

    プロフィール画像用のカスタムウィジェット追加

--- raccoon/articleapp/forms.py
+++ raccoon/articleapp/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
 from django.forms import ModelForm
 
 from .models import User
+from .widgets import ClearableFileInput
 
 
 class UserCreationForm(BaseUserCreationForm):
@@ -13,3 +14,6 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ["username", "display_name", "profile_image"]
+        widgets = {
+            "profile_image": ClearableFileInput(),
+        }
