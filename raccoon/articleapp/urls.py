bd3a1e78fd5ace2bc8af2debdc4acaf8cea9fc0b
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-12-01 03:58:10 +0900

    デフォルトの設定ページのurlのリダイレクト先をプロフィール更新ページにする

--- raccoon/articleapp/urls.py
+++ raccoon/articleapp/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path, reverse_lazy
+from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
@@ -20,6 +21,11 @@ urlpatterns = [
         name="login",
     ),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "settings/",
+        RedirectView.as_view(pattern_name="user_settings_profile"),
+        name="user_settings",
+    ),
     path(
         "settings/profile/",
         views.user_settings,
