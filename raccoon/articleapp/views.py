429a96687e67f737aa25d3598ea3c0174a61d293
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-12-05 18:57:57 +0900

    退会ページ追加

--- raccoon/articleapp/views.py
+++ raccoon/articleapp/views.py
@@ -1,7 +1,7 @@
 import datetime
 import random
 
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.core.paginator import Paginator
 from django.db.models import Count, Q
 from django.http import JsonResponse
@@ -277,3 +277,14 @@ def user_settings(request, current_menu_item="profile"):
 
     context["form"] = form
     return render(request, "articleapp/user_settings.html", context)
+
+
+@login_required
+def deactivate(request):
+    if request.method == "POST":
+        user = request.user
+        logout(request)
+        user.deactivate()
+
+        return redirect("index")
+    return render(request, "articleapp/deactivate.html")
