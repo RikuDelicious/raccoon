7f85ccd971a2a751766e9da2d83c6fbd51042a81
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-12-07 11:29:48 +0900

    投稿本文のマークダウンのプレビュー追加

---
+++ raccoon/articleapp/static/articleapp/post_create.js
@@ -0,0 +1,22 @@
+'use strict';
+{
+    $(() => {
+        const form = $('#post_form');
+
+        /* markedでbodyフィールドのマークダウンのプレビューを表示する */
+        const body_textarea = form.find('textarea[name=body]')
+        const body_preview = $('#body_preview');
+        marked.setOptions({
+            breaks: true,
+            highlight: function(code, lang) {
+                const language = hljs.getLanguage(lang) ? lang: 'plaintext';
+                return hljs.highlight(code, { language }).value;
+            },
+            langPrefix: 'hljs language-'
+        });
+
+        body_textarea.on('input', (e) => {
+            body_preview.html(marked.parse($(e.target).val()));
+        });
+    });
+}
