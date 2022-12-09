01cbb6c5f85b931fc298c346bb7d70e0cc03ffd8
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-12-10 03:53:53 +0900

    投稿作成ページテスト追加、完了

--- raccoon/articleapp/tests/views/test_post_create.py
+++ raccoon/articleapp/tests/views/test_post_create.py
@@ -1,8 +1,11 @@
+import string
+import random
+from urllib.parse import urlparse
+
+from articleapp.forms import PostForm
+from articleapp.models import Post, Tag, User
 from django.test import Client, TestCase
 from django.urls import reverse
-from urllib.parse import urlparse
-from articleapp.models import Post, Tag, User
-from articleapp.forms import PostForm
 from django.utils import timezone
 
 
@@ -130,3 +133,205 @@ class PostCreateViewTests(TestCase):
         )
         self.assertTrue(post.is_published)
         self.assertEqual(post.date_publish, timezone.localtime().date())
+
+    def test_エラーでページ再表示(self):
+        c = Client()
+        c.login(username="testuser_0", password="testuser_0")
+
+        # フォーム取得
+        response = c.get(self.url)
+        form = response.context["form"]
+
+        # フォーム送信
+        post_data = {
+            # titleフィールドを除外
+            "tags_text": "Python Django",
+            "slug": form.get_initial_for_field(form.fields["slug"], "slug"),
+            "body": "投稿本文",
+            "save_option": "save_and_publish",
+        }
+        response = c.post(self.url, post_data)
+        self.assertEqual(response.status_code, 400)
+        self.assertIsInstance(response.context["form"], PostForm)
+        self.assertIsNot(response.context["form"].errors.as_data, {})
+        self.assertFalse(
+            Post.objects.filter(user=self.users[0], slug=post_data["slug"]).exists()
+        )
+
+    def test_フォーム_titleフィールド(self):
+        c = Client()
+        c.login(username="testuser_0", password="testuser_0")
+
+        # 入力パターン
+        seed = string.ascii_lowercase
+        titles = [
+            "",  # 空
+            "a",  # 1文字
+            "".join(
+                random.choices(seed, k=Post._meta.get_field("title").max_length)
+            ),  # 最大文字数
+            "".join(
+                random.choices(seed, k=(Post._meta.get_field("title").max_length + 1))
+            ),  # 最大文字数 + 1
+        ]
+
+        for i, title in enumerate(titles):
+            # フォーム取得
+            response = c.get(self.url)
+            form = response.context["form"]
+
+            # フォーム送信
+            post_data = {
+                "title": title,
+                "tags_text": "Python Django",
+                "slug": form.get_initial_for_field(form.fields["slug"], "slug"),
+                "body": "投稿本文",
+                "save_option": "save_and_publish",
+            }
+            response = c.post(self.url, post_data)
+            if i in [0, 3]:
+                self.assertEqual(response.status_code, 400)
+            else:
+                self.assertEqual(response.status_code, 302)
+                redirect_url = urlparse(response.headers["Location"])
+                self.assertEqual(
+                    redirect_url.path,
+                    reverse("user_home", kwargs={"username": "testuser_0"}),
+                )
+
+    def test_フォーム_tags_textフィールド(self):
+        c = Client()
+        c.login(username="testuser_0", password="testuser_0")
+
+        # 入力パターン
+        seed = string.ascii_lowercase
+        tags_text_list = [
+            "",  # 空
+            "a",  # 1文字
+            "C# .Net",  # 複数のタグ
+            "C# .Net",  # 生成済みのタグ
+            ".Net C# WPF",  # 生成済みのタグ + 新規タグ
+            "".join(
+                random.choices(seed, k=Tag._meta.get_field("name").max_length)
+            ),  # タグの最大文字数
+            "".join(
+                random.choices(seed, k=(Tag._meta.get_field("name").max_length + 1))
+            ),  # タグの最大文字数 + 1
+            " ".join(
+                random.choices(seed, k=(PostForm().fields["tags_text"].max_length // 2))
+            ),  # フォームの最大文字数
+            " ".join(
+                random.choices(
+                    seed, k=(PostForm().fields["tags_text"].max_length // 2 + 1)
+                )
+            ),  # フォームの最大文字数 + 1
+        ]
+
+        for i, tags_text in enumerate(tags_text_list):
+            # フォーム取得
+            response = c.get(self.url)
+            form = response.context["form"]
+
+            # フォーム送信
+            post_data = {
+                "title": "タイトル",
+                "tags_text": tags_text,
+                "slug": form.get_initial_for_field(form.fields["slug"], "slug"),
+                "body": "投稿本文",
+                "save_option": "save_and_publish",
+            }
+            response = c.post(self.url, post_data)
+            if i in [6, 8]:
+                self.assertEqual(response.status_code, 400)
+            else:
+                self.assertEqual(response.status_code, 302)
+                redirect_url = urlparse(response.headers["Location"])
+                self.assertEqual(
+                    redirect_url.path,
+                    reverse("user_home", kwargs={"username": "testuser_0"}),
+                )
+
+        # タグが生成されていることの確認
+        self.assertEqual(Tag.objects.filter(name="C#").count(), 1)
+        self.assertEqual(Tag.objects.filter(name=".Net").count(), 1)
+        self.assertEqual(Tag.objects.filter(name="WPF").count(), 1)
+        self.assertEqual(Tag.objects.filter(name=tags_text_list[5]).count(), 1)
+        self.assertEqual(Tag.objects.filter(name=tags_text_list[6]).count(), 0)
+
+    def test_フォーム_slugフィールド(self):
+        c = Client()
+        c.login(username="testuser_0", password="testuser_0")
+
+        # 入力パターン
+        seed = string.ascii_lowercase + string.digits
+        slugs = [
+            None,  # 初期値
+            "",  # 空
+            "a",  # 1文字
+            "a",  # 既存のスラッグと同じ値
+            "".join(
+                random.choices(seed, k=Post._meta.get_field("slug").max_length)
+            ),  # スラッグの最大文字数
+            "".join(
+                random.choices(seed, k=(Post._meta.get_field("slug").max_length + 1))
+            ),  # スラッグの最大文字数 + 1
+        ]
+
+        for i, slug in enumerate(slugs):
+            # フォーム取得
+            response = c.get(self.url)
+            form = response.context["form"]
+
+            # フォーム送信
+            post_data = {
+                "title": "タイトル",
+                "tags_text": "Python",
+                "slug": slug
+                if slug is not None
+                else form.get_initial_for_field(form.fields["slug"], "slug"),
+                "body": "投稿本文",
+                "save_option": "save_and_publish",
+            }
+            response = c.post(self.url, post_data)
+
+            if i in [1, 3, 5]:
+                self.assertEqual(response.status_code, 400)
+            else:
+                self.assertEqual(response.status_code, 302)
+                redirect_url = urlparse(response.headers["Location"])
+                self.assertEqual(
+                    redirect_url.path,
+                    reverse("user_home", kwargs={"username": "testuser_0"}),
+                )
+    
+    def test_フォーム_bodyフィールド(self):
+        c = Client()
+        c.login(username="testuser_0", password="testuser_0")
+
+        # 入力パターン
+        bodies = [
+            "",  # 空
+            "a",  # 1文字
+        ]
+
+        for i, body in enumerate(bodies):
+            # フォーム取得
+            response = c.get(self.url)
+            form = response.context["form"]
+
+            # フォーム送信
+            post_data = {
+                "title": "タイトル",
+                "tags_text": "Python",
+                "slug": form.get_initial_for_field(form.fields["slug"], "slug"),
+                "body": body,
+                "save_option": "save_and_publish",
+            }
+            response = c.post(self.url, post_data)
+
+            self.assertEqual(response.status_code, 302)
+            redirect_url = urlparse(response.headers["Location"])
+            self.assertEqual(
+                redirect_url.path,
+                reverse("user_home", kwargs={"username": "testuser_0"}),
+            )
