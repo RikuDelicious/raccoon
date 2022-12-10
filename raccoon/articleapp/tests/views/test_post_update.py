0283d992d484de64ee2e39fad22839373d27ac54
Riku <101910815+RikuDelicious@users.noreply.github.com>
2022-12-11 01:08:45 +0900

    自動整形

--- raccoon/articleapp/tests/views/test_post_update.py
+++ raccoon/articleapp/tests/views/test_post_update.py
@@ -8,23 +8,26 @@ from django.test import Client, TestCase
 from django.urls import reverse
 from django.utils import timezone
 
+
 class PostUpdateTests(TestCase):
     def setUp(self):
         self.users = [
             User.objects.create_user(username=f"testuser_{i}", password=f"testuser_{i}")
             for i in range(2)
         ]
-    
+
     def test_ページアクセス(self):
         c = Client()
         post = Post.objects.create(
-                title=f"post_0",
-                body=f"post_0_body",
-                user=self.users[0],
-                is_published=True,
-                date_publish=(timezone.now().date()),
-            )
-        url = reverse("post_update", kwargs={"username": post.user.username, "slug": post.slug})
+            title=f"post_0",
+            body=f"post_0_body",
+            user=self.users[0],
+            is_published=True,
+            date_publish=(timezone.now().date()),
+        )
+        url = reverse(
+            "post_update", kwargs={"username": post.user.username, "slug": post.slug}
+        )
         # 未ログインでアクセスするとログインページにリダイレクト
         response = c.get(url)
         self.assertEqual(response.status_code, 302)
@@ -37,35 +40,41 @@ class PostUpdateTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # 投稿が存在しない場合は404
-        url = reverse("post_update", kwargs={"username": post.user.username, "slug": "hogehoge"})
+        url = reverse(
+            "post_update", kwargs={"username": post.user.username, "slug": "hogehoge"}
+        )
         response = c.get(url)
         self.assertEqual(response.status_code, 404)
 
         # ログイン済みで他のユーザーの投稿にアクセスすると404
         post = Post.objects.create(
-                title=f"post_1",
-                body=f"post_1_body",
-                user=self.users[1],
-                is_published=True,
-                date_publish=(timezone.now().date()),
-            )
-        url = reverse("post_update", kwargs={"username": post.user.username, "slug": post.slug})
+            title=f"post_1",
+            body=f"post_1_body",
+            user=self.users[1],
+            is_published=True,
+            date_publish=(timezone.now().date()),
+        )
+        url = reverse(
+            "post_update", kwargs={"username": post.user.username, "slug": post.slug}
+        )
         response = c.get(url)
         self.assertEqual(response.status_code, 404)
-    
+
     def test_ページアクセス時のコンテクスト(self):
         c = Client()
         c.login(username="testuser_0", password="testuser_0")
         tags = [Tag.objects.create(name=f"tag_{i}") for i in range(5)]
         post = Post.objects.create(
-                title=f"post_0",
-                body=f"post_0_body",
-                user=self.users[0],
-                is_published=True,
-                date_publish=(timezone.now().date()),
-            )
+            title=f"post_0",
+            body=f"post_0_body",
+            user=self.users[0],
+            is_published=True,
+            date_publish=(timezone.now().date()),
+        )
         post.tags.add(*tags)
-        url = reverse("post_update", kwargs={"username": post.user.username, "slug": post.slug})
+        url = reverse(
+            "post_update", kwargs={"username": post.user.username, "slug": post.slug}
+        )
 
         response = c.get(url)
         self.assertEqual(response.context["form_action_url"], url)
@@ -73,6 +82,7 @@ class PostUpdateTests(TestCase):
         self.assertEqual(response.context["form"].instance, post)
 
         # tage_textフィールドに紐づけたタグが復元することを確認する
-        self.assertEqual(response.context["form"].initial["tags_text"], "tag_0 tag_1 tag_2 tag_3 tag_4")
-
-    
+        self.assertEqual(
+            response.context["form"].initial["tags_text"],
+            "tag_0 tag_1 tag_2 tag_3 tag_4",
+        )
